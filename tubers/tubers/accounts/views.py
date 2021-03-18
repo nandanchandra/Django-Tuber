190e554e6d6e538f010e7b9232ace4cf7c709727
Chandra Nandan <chandranandan.chandrakar@gmail.com>
2021-03-18 18:58:04 +0530

    warrring_updated

--- tubers/tubers/accounts/views.py
+++ tubers/tubers/accounts/views.py
@@ -16,10 +16,10 @@ def login(request):
 
         if user is not None:
             auth.login(request,user)
-            messages.error(request,'you are logged in')
+            messages.warning(request,'you are logged in')
             return redirect('dashboard')
         else:
-            messages.error(request,'invalid credentials')
+            messages.warning(request,'invalid credentials')
             return redirect('login')
 
     return render(request, 'accounts/login.html')
@@ -37,11 +37,11 @@ def register(request):
 
         if password == confirm_password:
             if User.objects.filter(username=username).exists():
-                messages.error(request, 'Username Exits')
+                messages.warning(request, 'Username Exits')
                 return redirect('register')
             else:
                 if User.objects.filter(email=email).exists():
-                    messages.error(request, 'Email already exists')
+                    messages.warning(request, 'Email already exists')
                     return redirect('register')
                 else:
                     user = User.objects.create_user(first_name=firstname, last_name=lastname, username=username,
@@ -51,7 +51,7 @@ def register(request):
                     return redirect('login')
 
         else:
-            messages.error(request, 'Password do not match')
+            messages.warning(request,'Password do not match')
             return redirect('register')
 
     return render(request, 'accounts/register.html')
